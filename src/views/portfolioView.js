06012a396a46d3b5299023a578ece2572577c068
Xaruzo <kurutozu@gmail.com>
2026-05-06 14:23:49 +0800

    Design: Add logo icon next to name in navigation bar

--- src/views/portfolioView.js
+++ src/views/portfolioView.js
@@ -6,7 +6,10 @@ export class PortfolioView {
     renderNav() {
         return `
             <nav>
-                <a href="#home" class="logo">Xaru<span>zo</span></a>
+                <a href="#home" class="logo">
+                    <img src="images/logo.png" alt="Xaruzo Logo" class="nav-logo-img">
+                    Xaru<span>zo</span>
+                </a>
                 
                 <div class="nav-right">
                     <ul class="nav-links">
